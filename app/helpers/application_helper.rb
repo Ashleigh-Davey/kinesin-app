a8b2e8f309fc356f8453abb90482ed6c84355bd0
Ashleigh Davey <ashleigh.davey@ymail.com>
2022-06-01 17:56:19 +0100

    Add routes to navbar

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,15 @@
 module ApplicationHelper
+  # Given the name of an isoform, return the css class that the isoform badge should be
+  def isoform_class(isoform)
+    case isoform
+    when 'KIF5A'
+      'bg-kif5a'
+    when 'KIF5C'
+      'bg-success'
+    when 'KHC'
+      'bg-warning text-dark'
+    else
+      'bg-secondary'
+    end
+  end
 end
