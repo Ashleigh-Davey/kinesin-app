8215966acafa6cb85e682f26749e8facc2fb9be2
Ashleigh Davey <ashleigh.davey@ymail.com>
2022-11-21 18:09:29 +0000

    Add data export, navbar logo

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   root 'pages#home'
   get 'about', to: 'pages#about'
+  get 'submit', to: 'pages#submit'
   resources :mutations, only: [:show, :index] #only use show +index  action
 end
